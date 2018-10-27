821a7eb851e0ae7c48f892528ffc1fc1a1a3e8c2
Liza Stark <lizastark@gmail.com>
2018-10-27 15:57:26 -0400

    moved

--- Learning_Python/exercises_1-2/hello_world.py
+++
@@ -1,5 +0,0 @@
-message = "Hello world"
-print(message)
-
-message = "Hello python world"
-print(message)
--- Learning_Python/exercises_1-2/simple_message.py
+++
@@ -1,9 +0,0 @@
-idea = "stop being so hard on yourself"
-print(idea)
-
-idea1 = "develop curiosity instead"
-print(idea1)
-
-print(idea + " " + idea1)
-
-import this
